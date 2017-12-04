2d21d8213849db2ce77e5dfaef124df06bb13d61
Юнона Поспелова <iunasha@mail.ru>
2017-12-04 14:12:09 +0300

    Nothing special

--- compute.py
+++ compute.py
@@ -1,5 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from prody import *
+from ref_coords import *
+import os
+import glob
+
+
+def nucleic_test(pdb_file):
+
+
+
+
 
 def compute(PDB_index):
     t = np.arange(0., 2., 0.1)
