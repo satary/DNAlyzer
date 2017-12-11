0cbbdcd4230acb0241cfd233fd6549c77d72b34a
Юнона Поспелова <iunasha@mail.ru>
2017-12-11 19:36:24 +0300

    got functions for downloading

--- compute.py
+++ compute.py
@@ -2,15 +2,10 @@ import matplotlib.pyplot as plt
 plt.ioff()
 import json
 from threading import Lock
-lock=Lock() 
-
-import mpld3
-
+lock=Lock()
 import numpy as np
-from prody import *
-from ref_coords import *
-import os, glob
-import urllib # downloading file
+import os
+import urllib.request, uuid, shutil # used in saving_file function
 
 
 
@@ -25,31 +20,43 @@ def graph(PBD_index):
     return mpld3.draw_fig() 
 
     
-
+unique_directory_name = str(uuid.uuid4()) # creates unique directory name
 
 def saving_file(PDB_index):
     '''
-    This function creates temporary path
-    saves there fetched PDB file
+    This function creates temporary path with unique
+    name and saves there fetched PDB file
     '''
-    url = 'https://files.rcsb.org/download/%s.pdb' % PDB_index
-    if not os.path.isdir('DNAlyzer')
-        os.mkdir('DNAlyzer')
-    else:
-        for filename in glob.glob(os.path.join('analyzer', '*.pdb')):
-            os.remove('analyzer', filename) # я еще не поняла, что написать в else
-    testfile = urllib.URLopener()
-    testfile.retrieve(url, str(time.time()) + '.pdb') # это просто сохраняет файл, но не в папку, хз куда
+    with lock:
+        if not os.path.isdir(unique_directory_name): # creates directory with unique name
+            os.mkdir(unique_directory_name)
+        url = 'https://files.rcsb.org/download/%s.pdb' % PDB_index
+        os.chdir(unique_directory_name) # changing directory to new one
+        with urllib.request.urlopen(url) as response, open('%s.pdb' % PDB_index, 'wb' ) as out_file:
+            shutil.copyfileobj(response, out_file) # download the file from `url` and save it locally under `file_name` (open)
 
-
-def nucleic_test(pdb_file):
+def nucleic_test(PDB_index):
     '''
     This function tests PDB file for
     DNA chains in it.
     '''
+    dna = parsePDB('%s.pdb' % PDB_index)
+    dna_selection = dna.selec('nucleic')
+    if dna_selection == 0:
+        return ""
+    else: None
+    resids = np.unique(dna.getResnums())
+    if (resids.size % 2) == 0
+        return ""
+    else: None
+# better use try except
 
 
-
-
+def deleting_directory():
+    '''
+    This function deletes created directory
+    '''
+    os.chdir("../")
+    shutil.rmtree("/%s" % unique_directory_name)
 
     
